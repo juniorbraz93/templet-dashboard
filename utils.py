ef2544abc8af8501653ca2f4fd7dd4f36cd98bf0
juniorbraz93 <jbraz.est@gmail.com>
2025-04-09 13:22:01 -0300

    structuring of files

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 def get_top_n(df, column_name, n=25):
     top_n = df[column_name].nlargest(n).reset_index()
+    
     col_name_pt = {
-    'Sales': 'Vendas',
-    'Revenue': 'Receita'
+        'Sales': 'Vendas',
+        'Revenue': 'Receita'
     }.get(column_name, column_name)
+
     top_n.columns = ['Data', col_name_pt]
     top_n['Data'] = top_n['Data'].dt.strftime('%d/%m/%Y')
+
+    # Formatar valores da receita com 2 casas decimais
+    if col_name_pt == 'Receita':
+        top_n['Receita'] = top_n['Receita'].map(lambda x: f"{x:.2f}")
+
     return top_n
