02e91a630ad581d3769ae24f16a772f77a65e414
Junior Braz <54958766+juniorbraz93@users.noreply.github.com>
2025-04-11 09:37:43 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,9 +1,6 @@
-import dash_html_components as html
-import dash_core_components as dcc
-import dash_table
+from dash import html, dcc, dash_table
 from data import vendedores, df
 from data import gerar_vendas_fakes
-import dash_table
 
 
 layout = html.Div([
